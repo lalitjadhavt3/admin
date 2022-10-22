5075572fe59e5972879ff015a9e74548e8f46530
Priyankagit9 <agavanepriyanka09@gmail.com>
2022-10-22 15:17:00 +0530

    added design of multiple column

--- src/views/ui-elements/page-layouts/LayoutEmpty.js
+++ src/views/ui-elements/page-layouts/LayoutEmpty.js
@@ -1,5 +1,6 @@
 // ** React Imports
 import { Fragment } from 'react'
+import MultipleColumnForm from '../../forms/form-layouts/MultipleColumnForm';
 
 // ** Custom Components
 import Breadcrumbs from '@components/breadcrumbs'
@@ -13,22 +14,7 @@ for (var i = 0; i < iframes.length; i++) {
 const LayoutEmpty = () => {
   return (
     <Fragment>
-      <Breadcrumbs breadCrumbTitle='Layout Empty' breadCrumbParent='Layouts' breadCrumbActive='Layout Empty' />
-      <Alert color='primary'>
-        <div className='alert-body'>
-          <span className='fw-bold'>Info: </span>
-          <span>
-            This layout can be useful for getting started with empty content section. Please check the{' '}
-            <a
-              href='https://pixinvent.com/demo/vuexy-react-admin-dashboard-template/documentation/docs/development/page-layouts'
-              target='_blank'
-            >
-              Layout empty documentation
-            </a>{' '}
-            for more details.
-          </span>
-        </div>
-      </Alert>
+      <MultipleColumnForm />
     </Fragment>
   )
 }
